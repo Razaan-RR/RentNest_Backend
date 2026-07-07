de8012d094c966e2cf541a62d3f255fe8b472ad6
Razaan Reza <razaanreza0705@gmail.com>
2026-07-07 23:42:03 +0600

    project setup

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+// import { userRoutes } from "./modules/user/user.route";
+import config from "./config";
+
+
+
+const app : Application = express();
+
+app.use(cors({
+    origin : config.app_url,
+    credentials : true,
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+app.use(cookieParser())
+
+
+app.get("/",(req : Request, res : Response) => {
+    res.send("Hello, World!");
+});
+
+// app.post()
+
+// app.use("/api/users", userRoutes)
+
+export default app;
